c29796ea7e31b5b52335c493a080bd6941248585
jiangwei6 <jiangwei6@huawei.com>
2024-04-26 15:51:29 +0800

    Update EventButton.vue

--- src/components/EventButton.vue
+++ src/components/EventButton.vue
@@ -3,16 +3,12 @@ const props = defineProps<{
   globalData: Record<string, any>;
   setGlobalData: (newValue: any) => void;
   public_id: string;
-  // event_str: string;
   btn_str: string;
   var1: string;
   var2: string
 }>();
 function evalFunc() {
   console.log('dispatchEvent');
-  // window.dispatchEvent(new CustomEvent (props.event_str, {
-  //   detail: { publicId: props.public_id }
-  // }));
   window.dispatchEvent(new CustomEvent('automa:execute-workflow', {
 	detail: { 
 		publicId: props.public_id,
